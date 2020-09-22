aca608935f97c1e9253171890c6f5610968368ea
Vitor <vitordelavedova@hotmail.com>
2020-09-22 20:09:57 -0300

    Adicionando validacoes

--- calc_Idade.js
+++ calc_Idade.js
@@ -7,11 +7,21 @@ var botaoLimpar = document.getElementById('botaoLimpar');
 botaoLimpar.addEventListener('click', limpar);
 
 function calc_Idade() {
-    var ano = campoAno.value.trim();
+    var ano = parseInt(campoAno.value);
     var idade = new Date().getFullYear() - ano;
-    var mensagem = 'Idade: ' + idade + ' ano(s).';
+    
+    if(isNaN(ano)) {
+        alert('Ano de nascimento inválido');
+        campoAno.value = '';
+    } else if(ano > new Date().getFullYear()) {
+        alert('Ano de nascimento maior que o ano atual');
+        campoAno.value = '';
+    } else {
+        var mensagem = 'Idade: ' + idade + ' ano(s).';
     mensagemResult.innerText = mensagem;
+    }
 }
+    
 function limpar() {
     campoAno.value = '';
     mensagemResult.innerText = '';
